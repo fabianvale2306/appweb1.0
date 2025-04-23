5c3598bca1a6e7d40440fd38187c58be48b6f51f
fabian <fabianvaleriano53@gmail.com>
2025-04-23 11:19:55 -0500

    Carga de Clientes

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using appweb1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace appweb1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+namespace appweb1.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string DNI { get; set; }
+    }
+}
