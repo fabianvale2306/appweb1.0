5f9416d8058c05b271d270932aec46bdf6e95552
fabian <fabianvaleriano53@gmail.com>
2025-04-24 11:37:29 -0500

    se agregaron cambios

--- Pages/Clientes/Create.cshtml.cs
+++ Pages/Clientes/Create.cshtml.cs
@@ -1,12 +1,26 @@
+using appweb1.Data;
+using appweb1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace MyApp.Namespace
 {
     public class CreateModel : PageModel
     {
+        private readonly AppDbContext _ctx;
+        public CreateModel(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        [BindProperty]
+        public Cliente Cliente { get; set; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            _ctx.Clientes.Add(Cliente);
+            _ctx.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
